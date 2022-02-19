dd0e82e9282a76359d890d8d4ccb97712601959e
sejalpalande <96038310+sejalpalande@users.noreply.github.com>
2022-02-19 18:20:59 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebExam.Models
+{
+    public class Product
+    {
+        internal int ProductId;
+
+        public int ProductID { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Product Name.")]
+        public String ProductName { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Rate.")]
+        public decimal Rate { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Product's Description.")]
+        public String Description { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Entee Product's Category Name.")]
+        public String CategoryName { get; set; }
+    }
+}
